3bc89041efc77501c984e529d5770f24569bee61
Ali <my7box@yahoo.com>
2023-03-11 20:37:58 +0330

    Add files via upload

--- app/page/gateway/request.php
+++ app/page/gateway/request.php
@@ -1,14 +1,18 @@
 <?php
-	
+echo '<p>-------gatway/request strart---</p>'	;
 	include("app/config.php");
+
 	require_once('app/page/gateway/lib/nusoap.php');
 	//Factor Get
+
 	if(empty($_POST['id'])) { header("location: ?index=index&error=1"); }
 	else {
 
-	$zng_fg = mysql_query("SELECT * FROM zng_factor WHERE ID='$_POST[id]'");
-	if(mysql_num_rows($zng_fg) < 1) { header("location: ?index=index&error=2"); }
-	while($row = mysql_fetch_array($zng_fg))
+	//$zng_fg = mysql_query("SELECT * FROM zng_factor WHERE ID='$_POST[id]'");
+	$query = "SELECT * FROM zng_factor WHERE ID =".$_POST['id'];
+	$zng_fg = mysqli_query($zng_info_data, $query);
+	if(mysqli_num_rows($zng_fg) < 1) { header("location: ?index=index&error=2"); }
+	while($row = mysqli_fetch_assoc($zng_fg))
 	{
 	$zng_id = $row['ID'];
 	$zng_from = $row['from'];
@@ -20,37 +24,30 @@
 	$zng_description = $row['description'];
 	$zng_status = $row['status'];
 	$zng_ref = $row['ref'];
+
 	}
 	
 	$zng_paytime_compare = str_replace("/","",$zng_paytime);
 	if($zng_paytime_compare<$zng_date_compare || $zng_status=="paid") { header("location: ?index=index&error=5"); }
 
 	else {
+
 			$MerchantID = $zng_zarinpal_mc; //Required
 	$Amount = $zng_price; //Amount will be based on Toman  - Required
 	$Description = 'پرداخت فاکتور شماره:' . $zng_id;  // Required
 	$Email = $_POST['u_email']; // Optional
 	$Mobile = $_POST['u_phone']; // Optional
-	$u_info_array = array("u_email"=>$_POST[u_email],"u_phone"=>$_POST[u_phone]);
+	$u_info_array = array("u_email"=>$_POST['u_email'],"u_phone"=>$_POST['u_phone']);
 	$u_info_json = json_encode($u_info_array);
 	$u_info_base = base64_encode($u_info_json);
-	$CallbackURL = $zng_web_address . '?index=verify&id=' . $_POST[id] . '&u_info=' . $u_info_base;  // Required
-	
+	$CallbackURL = $zng_web_address . '?index=verify&id=' . $_POST['id'] . '&u_info=' . $u_info_base;  // Required
 	
 	// URL also Can be https://ir.zarinpal.com/pg/services/WebGate/wsdl
-	$client = new nusoap_client('https://de.zarinpal.com/pg/services/WebGate/wsdl', 'wsdl'); 
+	$client1 = new nusoap_client('https://de.zarinpal.com/pg/services/WebGate/wsdl', 'wsdl');
+	$client = new nusoap_client('https://ir.zarinpal.com/pg/services/WebGate/wsdl', 'wsdl'); 
 	$client->soap_defencoding = 'UTF-8';
-	$result = $client->call('PaymentRequest', array(
-													array(
-															'MerchantID' 	=> $MerchantID,
-															'Amount' 		=> $Amount,
-															'Description' 	=> $Description,
-															'Email' 		=> $Email,
-															'Mobile' 		=> $Mobile,
-															'CallbackURL' 	=> $CallbackURL
-														)
-													)
-	);
+	$result = $client->call('PaymentRequest', array( array('MerchantID'=> $MerchantID,'Amount'=> $Amount,'Description'=> $Description,'Email'=>$Email,'Mobile'=> $Mobile,'CallbackURL'=> $CallbackURL)));
+	echo $result['Authority'];
 	
 	//Redirect to URL You can do it also by creating a form
 	if($result['Status'] == 100)
@@ -62,4 +59,4 @@
 	}
 
 	}
-?>
+echo '<p>-------end of gatway/request---</p>';
--- app/page/gateway/verify.php
+++ app/page/gateway/verify.php
@@ -6,9 +6,11 @@
 	if(empty($_GET['id'])) { header("location: ?index=index&error=1"); }
 	else {
 
-	$zng_fg = mysql_query("SELECT * FROM zng_factor WHERE ID='$_GET[id]'");
-	if(mysql_num_rows($zng_fg) < 1) { header("location: ?index=index&error=2"); }
-	while($row = mysql_fetch_array($zng_fg))
+	//$zng_fg = mysql_query("SELECT * FROM zng_factor WHERE ID='$_GET[id]'");
+	$query = "SELECT * FROM zng_factor WHERE ID =".$_GET['id'];
+	$zng_fg = mysqli_query($zng_info_data, $query);
+	if(mysqli_num_rows($zng_fg) < 1) { header("location: ?index=index&error=2"); }
+	while($row = mysqli_fetch_assoc($zng_fg))
 	{
 	$zng_id = $row['ID'];
 	$zng_from = $row['from'];
@@ -45,7 +47,7 @@
 			$u_json_decode = json_decode($u_info_debase);
 			$u_info_email = $u_json_decode->{'u_email'};
 			$u_info_phone = $u_json_decode->{'u_phone'};
-			mysql_query("UPDATE `zng_factor` SET `status`='paid',`ref`='$result[RefID]', `email`='$u_info_email', `phone`='$u_info_phone' WHERE ID='$_GET[id]'");
+			mysqli_query($zng_info_data,"UPDATE `zng_factor` SET `status`='paid',`ref`='".$result['RefID']."', `email`='".$u_info_email."', `phone`='".$u_info_phone."' WHERE ID='".$_GET['id']);
 			header("location: ?index=id&id=$_GET[id]&ref=$result[RefID]");
 		} else {
 			header("location: ?index=index&error=3&ref=$result[Status]");
